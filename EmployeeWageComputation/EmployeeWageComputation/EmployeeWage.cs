375a5a91f9fc2d46be2d38fd1d95ea347abe349c
Sampada <sampadahakke102@gmail.com>
2022-02-08 22:41:28 +0530

    Adding Employee Attendance

---
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class EmployeeWage
+    {
+        const int IS_FULL_TIME = 1;
+        public void EmployeeAttendance()
+        {
+
+            Random random = new Random();
+            int EmpCheck = random.Next(0, 2);
+
+            if (EmpCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            Console.ReadLine();
+        }
+    }
+}
