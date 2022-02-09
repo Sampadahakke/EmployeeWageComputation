7f2ec06eb88be1216f53bcca49a9f9bd41d4a498
Sampadahakke <96908429+Sampadahakke@users.noreply.github.com>
2022-02-09 15:05:39 +0530

    Update Program.cs

--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -13,8 +13,6 @@ namespace EmployeeWageComputation
             Console.WriteLine("Welcome to Employee Wage Computation");
             EmployeeWage employee = new EmployeeWage();
             employee.EmployeeAttendance();
-            employee.DailyWage();
-            employee.PartTimeEmpWage();
             employee.SwitchCase();
             Console.ReadLine();
         }
